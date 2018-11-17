9578c9526ebbca302d37693b111e5ca6816c3a3c
Certamin <45127818+Certamin@users.noreply.github.com>
2018-11-17 14:04:08 -0800

    Created separate flasks file

--- Main.py
+++ Main.py
@@ -1,20 +1,7 @@
 import praw
-import secrets
-import string
-from flask import Flask, render_template
-import requests
 
 class Reddit:
     def __init__(self):
-        app = Flask(__name__)
-        @app.route(/)
-
-        client_id = "25EcHoX8LS14rg"
-        random_string = ''.join(secrets.choice(string.ascii_uppercase + string.digits) for _ in range(6))
-
-        url = "https://www.reddit.com/api/v1/authorize?client_id=" + client_id + \
-              "&response_type=\"code\"&state=\"" + random_string + \
-              "\"&redirect_uri=\"localhost:8080\"&duration=\"temporary\"&scope=[]"
 
         subscribedList = []
 
