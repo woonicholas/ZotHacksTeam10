f83670a562c89cd28e5b2a5e8c539ed22192729d
Certamin <45127818+Certamin@users.noreply.github.com>
2018-11-17 19:10:27 -0800

    Cosolidated

--- FlaskMain.py
+++ FlaskMain.py
@@ -1,14 +1,20 @@
 from flask import Flask, render_template, jsonify, request
 import facebook
+import TwitterMain
+import all
 app = Flask(__name__)
 
-@app.route("/main/", methods=["GET"])
+@app.route("/main/process", methods=["GET"])
+def sendData():
+    request.POST(all.process())
 
 @app.route("/facebook/process", methods=["GET"])
 def sendData():
-    request.POST(facebook.process(facebook.get_posts()))
+    request.POST(facebook.get_processed_data())
 
-@app.route("/main/", methods=["GET"])
+@app.route("/twitter/process", methods=["GET"])
+def sendData():
+    request.POST(TwitterMain.process())
 
 @app.route("/main/", methods=["GET"])
 
