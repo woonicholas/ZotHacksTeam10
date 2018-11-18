d99aa45f681fc488146cd6db1d4899f27259aa15
Greta Yu <gretayu00@gmail.com>
2018-11-17 18:25:32 -0800

    standard times

---
+++ twitterToFacebookTimes.py
@@ -0,0 +1,30 @@
+def getNumMonth(month):
+    print(month)
+    if month=="Jan":
+        return "01"
+    elif month=="Feb":
+        return "02"
+    elif month=="Mar":
+        return "03"
+    elif month=="Apr":
+        return "04"
+    elif month=="May":
+        return "05"
+    elif month=="Jun":
+        return "06"
+    elif month=="Jul":
+        return "07"
+    elif month=="Aug":
+        return "08"
+    elif month=="Sep":
+        return "09"
+    elif month=="Oct":
+        return "10"
+    elif month=="Nov":
+        return "11"
+    else:
+        return "12"
+
+def makeFacebookTime(time):
+    return time[-4:]+"-"+getNumMonth(time[4:7])+"-"+time[8:10]+"T"+time[11:19]+"+0000"
+    
