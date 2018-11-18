5555d970416e32fe788e809fd626dfb0dfde3708
Certamin <45127818+Certamin@users.noreply.github.com>
2018-11-17 18:01:08 -0800

    Completed Twitter

--- RedditMain.py
+++ RedditMain.py
@@ -1,4 +1,5 @@
 import praw
+import json
 
 class Reddit:
     def __init__(self):
@@ -9,7 +10,25 @@ class Reddit:
     def checkLogin(self, user, pw):
         if user == self.username and pw == self.password:
             self.authenticated = True
+            return "Authenticated"
+
+    def create_reddit_instance(self):
+        if self.authenticated:
+            with open("credentialsReddit.json", "r") as creds:
+                credentials = json.load(creds)
+            instance = praw.Reddit(client_id=credentials['client_id'],
+                                   client_secret=credentials['client_secret'],
+                                   username=self.username,
+                                   password=self.password,
+                                   user_agent=credentials['user_agent'])
+            stuff = instance.subreddit('all').new()
+            for thing in stuff:
 
 
+        else:
+            return "Error: Not Authenticated"
 
 
+x = Reddit()
+x.checkLogin("crsunlimit", "Aa123456")
+x.create_reddit_instance()
